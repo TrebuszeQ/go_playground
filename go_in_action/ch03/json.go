46d5ae6438381af307675eb04c214c6b4176516a
trebuszeq <trebuszeq@gmail.com>
2025-07-30 20:27:23 +0200

    structs

---
+++ go_in_action/ch03/json.go
@@ -0,0 +1,7 @@
+package main
+
+type Animal struct {
+	Name           string  `json:"animal_name"`
+	ScientificName string  `json:"scientific_name"`
+	Weight         float32 `json:"animal_average_weight`
+}

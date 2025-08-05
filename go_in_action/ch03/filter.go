f683cb005ef8ef3229720a9a5aad3160b494c220
trebuszeq <trebuszeq@gmail.com>
2025-08-05 13:40:13 +0200

    chapter 3 finished

---
+++ go_in_action/ch03/filter.go
@@ -0,0 +1,23 @@
+import (
+	"fmt"
+	"unicode"
+)
+
+func filter[T any](items []T, fx func(T) bool) []T {
+	var filtered []T
+	for _, v := range items {
+		if fx(v) {
+			filtered = append(filtered, v)
+		}
+	}
+	return filtered
+}
+
+func main() {
+	strings := []string{"My", "name", "Inigo", "Montoya"}
+
+	strings = filter[string](strings, func(s string) bool {
+		return unicode.IsUpper(rune(s[0]))
+	})
+	fmt.Println(strings)
+}

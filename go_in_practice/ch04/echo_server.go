c1141c7b4c6fc61babd2d5a6ea0a20b55152677c
trebuszeq <trebuszeq@gmail.com>
2025-08-11 11:53:56 +0200

    Ch4 finished

--- go_in_practice/ch04/echo_server.go
+++ go_in_practice/ch04/echo_server.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"net"
 )
 
 func response(data []byte, conn net.Conn) {
-	defer func() {
-		conn.Close()
-	}()
 	conn.Write(data)
+	panic(errors.New("pretend I'm a real error"))
 }
 
 func handle(conn net.Conn) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("Fatal error: %s", err)
+		}
+		conn.Close()
+	}()
 	reader := bufio.NewReader(conn)
 	data, err := reader.ReadBytes('\n')
 	if err != nil {
-		fmt.Println("Failed to read from socket.")
-		conn.Close()
+		fmt.Println("Failed to read from socket")
 	}
 	response(data, conn)
 }

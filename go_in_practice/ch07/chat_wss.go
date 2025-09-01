794cc9b70712f67a13dc119072bbc9ec3b8e9898
trebuszeq <trebuszeq@gmail.com>
2025-09-01 08:01:06 +0200

    chat_wss

---
+++ go_in_practice/ch07/chat_wss.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+
+	"golang.org/x/net/websocket"
+)
+
+var chars = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ")
+var clients map[string]*websocket.conn
+
+func init() {
+	clients = make(map[string]*websocket.conn)
+}
+
+func generateId() string {
+	r := make([]rune, 16)
+	for i := range r {
+		r[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(r)
+}
+
+func chatHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `
+			<!DOCTYPE html>
+				<html>
+				<head>
+					<title>Let's Chat</title>
+					<style>
+					#chat (
+						max-width: 400px;
+						margin: auto;
+						font-family: system-ui, sans-serif;
+					)
+					.message {
+						padding: 1rem 0.25rem;
+						border: 1px solid black;
+						margin-bottom: 0.5rem;
+					}
+					</style>
+				</head>
+				<body>
+					<div id="chat">
+					<h1>Chat</h1>
+					<div id="messages"></div>
+					<input id="message" autofocus type="text"
+					placeholder="Enter message..." />
+					<div>
+						<p>Chat members:</p>
+						<ul id="chat-members"></ul>
+					</div>
+					</div>
+				</body>
+				<script>
+					const text = document.GetElementById("message");
+					const messages = document.GetElementById("messages");
+					const members = document.GetElementById("chat-members");
+					const ws = new WebSocket("ws://localhost:8081/ws");
+					ws.onmessage = e => {
+						const msg = JSON.parse(e.data);
+						if (msg.message_type == "joinleave") {
+							members.innerHTML = "";
+							msg.chat_members.forEach(
+								member => {
+									const li = document.createElement("li");
+									li.innerHTML = member;
+									members.appendChild(li);
+								}
+							);
+							return;
+						}
+							if (msg.message_type === "message") {
+								const message = document.CreateELement("div");
+								message.classList.add("message");
+								message.innerHTML = msg.sender_id + " said: " + msg.message;
+								messages.appendChild(message);
+								return;
+							}
+					}
+					document.getElementById("message").addEventListener("keyup", 
+					e => {
+						if (e.keu == "Enter") {
+							ws.send(e.target.value);
+							message.value = "";
+						}
+					});
+				</script>
+				</body>
+				</html>
+	`)
+}
+
+type servermsg struct {
+	MessageType string   `json:"message_type"`
+	Message     string   `json:"message,omitempty"`
+	Id          string   `json:"id,omitempty"`
+	SenderID    string   `json:"sender_id,omitempty"`
+	ChatMembers []string `json:"chat_members"`
+}
+
+func compileChatMembers() []string {
+	var chatMembers []string
+	for k, _ := range clients {
+		chatMembers = append(chatMembers, k)
+	}
+	return chatMembers
+}
+
+func sendToClients(msg servermsg) error {
+	msgJSON, err := json.Marshal(msg)
+	if err != nil {
+		return nil
+	}
+	for k := range clients {
+		if err := websocket.Message.Send(clients[k], string(msgJSON)); err != nil {
+			return err
+		}
+	}
+	return nil
+}

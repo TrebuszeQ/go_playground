aa8e227c75b1fa858918964bf84d5c6dea3e8975
trebuszeq <trebuszeq@gmail.com>
2025-09-10 19:43:53 +0200

    routing

---
+++ go_in_practice/ch08/verb_routing.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+)
+
+func getComments(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("here you'll get the comments"))
+}
+
+func postComments(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("thank you for posting a comment"))
+}
+
+func main() {
+	http.HandleFunc("GET /comments", getComments)
+	http.HandleFunc("POST /comments", postComments)
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		panic(err)
+	}
+}
